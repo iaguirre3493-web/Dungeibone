c6c272b17761e9d661e59733ea27c567671feb98
Ibone Aguirre <iaguirre3493@gmail.com>
2026-05-05 19:14:32 +0200

    Crear pantalla inicial de Dungeibone

--- core/src/main/java/com/dam/dungeibone/FirstScreen.java
+++ core/src/main/java/com/dam/dungeibone/FirstScreen.java
@@ -1,45 +1,83 @@
 package com.dam.dungeibone;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+    private DungeiboneGame game;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private OrthographicCamera camera;
+
+    public FirstScreen() {
+    }
+
+    public FirstScreen(DungeiboneGame game) {
+        this.game = game;
+    }
+
     @Override
     public void show() {
-        // Prepare your screen here.
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+
+        font.getData().setScale(3);
+        font.draw(batch, "DUNGEIBONE", 260, 360);
+
+        font.getData().setScale(1.5f);
+        font.draw(batch, "Pulsa ENTER para comenzar", 250, 300);
+
+        font.getData().setScale(1);
+        font.draw(batch, "Version minima - libGDX", 310, 250);
+
+        batch.end();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+            System.out.println("Comenzar partida");
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
-        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
-        if(width <= 0 || height <= 0) return;
-
-        // Resize your screen here. The parameters represent the new window size.
     }
 
     @Override
     public void pause() {
-        // Invoked when your application is paused.
     }
 
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
     }
 
     @Override
     public void hide() {
-        // This method is called when another screen replaces this one.
     }
 
     @Override
     public void dispose() {
-        // Destroy screen's assets here.
+        batch.dispose();
+        font.dispose();
     }
 }
