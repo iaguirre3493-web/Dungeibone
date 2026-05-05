251f77cc4aa578c4d8d01249decd704f58d33e25
Ibone Aguirre <iaguirre3493@gmail.com>
2026-05-05 19:22:02 +0200

    Añadir llave puerta y cambio de nivel

--- core/src/main/java/com/dam/dungeibone/GameScreen.java
+++ core/src/main/java/com/dam/dungeibone/GameScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen implements Screen {
 
@@ -18,14 +19,18 @@ public class GameScreen implements Screen {
     private SpriteBatch batch;
     private BitmapFont font;
 
-    private float playerX;
-    private float playerY;
-    private float playerSize;
+    private Rectangle player;
+    private Rectangle key;
+    private Rectangle door;
+
     private float playerSpeed;
 
     private int vida;
     private int puntos;
     private int nivel;
+
+    private boolean tieneLlave;
+    private boolean keyVisible;
 
     public GameScreen(DungeiboneGame game) {
         this.game = game;
@@ -41,55 +46,86 @@ public class GameScreen implements Screen {
         font = new BitmapFont();
         font.setColor(Color.WHITE);
 
-        playerX = 400;
-        playerY = 300;
-        playerSize = 40;
-        playerSpeed = 220;
-
         vida = 100;
         puntos = 0;
         nivel = 1;
+        playerSpeed = 220;
+
+        cargarNivel(nivel);
+    }
+
+    private void cargarNivel(int numeroNivel) {
+        tieneLlave = false;
+        keyVisible = true;
+
+        player = new Rectangle(60, 60, 40, 40);
+
+        if (numeroNivel == 1) {
+            key = new Rectangle(360, 300, 25, 25);
+            door = new Rectangle(700, 500, 50, 70);
+        } else {
+            key = new Rectangle(650, 100, 25, 25);
+            door = new Rectangle(700, 500, 50, 70);
+        }
     }
 
     @Override
     public void render(float delta) {
         updatePlayer(delta);
+        checkCollisions();
         clearScreen();
-        drawPlayer();
+        drawGame();
         drawHUD();
     }
 
     private void updatePlayer(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
-            playerX -= playerSpeed * delta;
+            player.x -= playerSpeed * delta;
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
-            playerX += playerSpeed * delta;
+            player.x += playerSpeed * delta;
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
-            playerY += playerSpeed * delta;
+            player.y += playerSpeed * delta;
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
-            playerY -= playerSpeed * delta;
+            player.y -= playerSpeed * delta;
         }
 
-        if (playerX < 0) {
-            playerX = 0;
+        if (player.x < 0) {
+            player.x = 0;
         }
 
-        if (playerY < 0) {
-            playerY = 0;
+        if (player.y < 0) {
+            player.y = 0;
         }
 
-        if (playerX > 800 - playerSize) {
-            playerX = 800 - playerSize;
+        if (player.x > 800 - player.width) {
+            player.x = 800 - player.width;
         }
 
-        if (playerY > 600 - playerSize) {
-            playerY = 600 - playerSize;
+        if (player.y > 600 - player.height) {
+            player.y = 600 - player.height;
+        }
+    }
+
+    private void checkCollisions() {
+        if (keyVisible && player.overlaps(key)) {
+            keyVisible = false;
+            tieneLlave = true;
+            puntos += 100;
+        }
+
+        if (player.overlaps(door) && tieneLlave) {
+            if (nivel == 1) {
+                nivel = 2;
+                cargarNivel(nivel);
+            } else {
+                System.out.println("Victoria");
+            }
         }
     }
 
@@ -98,13 +134,26 @@ public class GameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
-    private void drawPlayer() {
+    private void drawGame() {
         camera.update();
         shapeRenderer.setProjectionMatrix(camera.combined);
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        shapeRenderer.setColor(Color.DARK_GRAY);
+        shapeRenderer.rect(0, 0, 800, 600);
+
         shapeRenderer.setColor(Color.GREEN);
-        shapeRenderer.rect(playerX, playerY, playerSize, playerSize);
+        shapeRenderer.rect(player.x, player.y, player.width, player.height);
+
+        if (keyVisible) {
+            shapeRenderer.setColor(Color.YELLOW);
+            shapeRenderer.rect(key.x, key.y, key.width, key.height);
+        }
+
+        shapeRenderer.setColor(Color.BROWN);
+        shapeRenderer.rect(door.x, door.y, door.width, door.height);
+
         shapeRenderer.end();
     }
 
@@ -116,6 +165,16 @@ public class GameScreen implements Screen {
         font.draw(batch, "Vida: " + vida, 20, 580);
         font.draw(batch, "Puntos: " + puntos, 20, 550);
         font.draw(batch, "Nivel: " + nivel, 20, 520);
+
+        if (tieneLlave) {
+            font.draw(batch, "Llave: SI", 20, 490);
+        } else {
+            font.draw(batch, "Llave: NO", 20, 490);
+        }
+
+        font.getData().setScale(1);
+        font.draw(batch, "Objetivo: recoge la llave y llega a la puerta", 250, 580);
+
         batch.end();
     }
 
